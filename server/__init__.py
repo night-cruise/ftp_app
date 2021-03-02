45144752dfe1c46c3d28fd0f8f402405b720e957
Night Cruising <2586447362@qq.com>
2021-03-02 22:55:15 +0800

    initial commit

---
+++ server/__init__.py
@@ -0,0 +1,10 @@
+#!/user/bin/python
+# -*-coding:UTF-8-*-
+""" 
+@Author:    Night Cruising
+@File:      __init__.py.py
+@Time:      2021/03/02
+@Version:   3.7.3
+@Desc:      None
+"""
+# here put the import lib
