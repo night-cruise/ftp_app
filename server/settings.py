29f12d75d7b7b854d5fb742775bcac15059c15e0
Night Cruising <2586447362@qq.com>
2021-03-03 17:35:17 +0800

    update settings

--- server/settings.py
+++ server/settings.py
@@ -11,13 +11,14 @@
 import os
 
 STATUS_CODE = {
+    '000': 'no message.',
     '200': 'Request success.',
     '201': 'Created success.',
     '400': 'Parameter error.',
-    '401': 'Unauthorized.',
-    '402': 'User has existed',
+    '401': 'Auth error, please login again.',
+    '402': 'User\dir\file has existed',
     '403': 'No permission.',
-    '404': 'Content not found.'
+    '404': 'Not found.'
 }
 
 DIST_QUOTA = 1024*1024*100
@@ -25,3 +26,4 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__)) + '\home'
 TEST_USER_DB = 'test.db'
 USER_DB = 'user.db' # dict structure
 IP_PORT = ('localhost', 9999)
+SECRET_KEY = os.getenv('SECRET_KEY', 'secret key')
